ce4e022b89190e7be358a77988a26d12cb2cf5c0
DerianPaez <derianpaez01@hotmail.com>
2021-08-15 16:41:32 -0500

    feat(frontend): offer button adding color

--- js/accordion.js
+++ js/accordion.js
@@ -8,50 +8,55 @@ const button3 = document.getElementById('button-3')
 const webDevelopment = {
   description: 'Desarrollo Web Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam harum quae at voluptates autem fugiat soluta, tempore dignissimos fuga nemo. Nulla autem itaque iste, qui suscipit nemo fugit dicta!',
   img: 'images/offer-web-development.png',
-  alt: 'Imagen de desarrollo web'
+  alt: 'Imagen de desarrollo web',
+  buttonId: button1
 }
 
 const responsiveDesign = {
   description: 'Diseño responsivo Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam harum quae at voluptates autem fugiat soluta, tempore dignissimos fuga nemo. Nulla autem itaque iste, qui suscipit nemo fugit dicta!',
   img: 'images/offer-responsive-design.svg',
-  alt: 'Imagen de diseño responsivo'
+  alt: 'Imagen de diseño responsivo',
+  buttonId: button2
 }
 
 const seo = {
   description: 'SEO Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam harum quae at voluptates autem fugiat soluta, tempore dignissimos fuga nemo. Nulla autem itaque iste, qui suscipit nemo fugit dicta!',
   img: 'images/offer-seo.svg',
-  alt: 'Imagen de SEO'
+  alt: 'Imagen de SEO',
+  buttonId: button3
 }
 
 let description = document.createElement('p')
 let img = document.createElement('img')
 
 function changeValues(value){
+  cleanContainers()
   description.innerHTML = value.description
   descriptionContainer.appendChild(description)
   img.setAttribute('src', value.img)
   img.setAttribute('alt', value.alt)
   imgContainer.appendChild(img)
+  value.buttonId.classList.add('offer-button-color')
 }
 
 function cleanContainers(){
   descriptionContainer.innerHTML = ''
   imgContainer.innerHTML = ''
+  button1.classList.remove('offer-button-color')
+  button2.classList.remove('offer-button-color')
+  button3.classList.remove('offer-button-color')
 }
 
 buttonsContainer.addEventListener('click', (event) => {
   let buttonPressed = event.target
   switch (buttonPressed) {
     case button1:
-      cleanContainers()
       changeValues(webDevelopment)
     break;
     case button2:
-      cleanContainers()
       changeValues(responsiveDesign)
     break;
     case button3:
-      cleanContainers()
       changeValues(seo)
     break;
 
