4dd7548a06436d761ee6ee0aaf60fcdd3981f2b7
James <mendozajames02@hotmail.com>
2021-08-14 23:30:03 -0500

    feat(frontend): add icon favicon & add image to about-us

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 const menuIcon = document.getElementById('menu-icon')
 const menuNav = document.getElementById('menu-nav')
 const menuIconClose = document.getElementById('menu-icon-close')
+const ancleList = document.querySelectorAll('#header-id a')
 
 menuIcon.addEventListener('click', () => {
   menuNav.classList.toggle('menu-nav-active')
@@ -8,4 +9,13 @@ menuIcon.addEventListener('click', () => {
 
 menuIconClose.addEventListener('click', () => {
   menuNav.classList.toggle('menu-nav-active')
+})
+
+ancleList.forEach(ancle => {
+  ancle.addEventListener('click', () => {
+    let isMenuActive = menuNav.classList.contains('menu-nav-active')
+    if(isMenuActive) {
+      menuNav.classList.remove('menu-nav-active')
+    }
+  })
 })
